e8067d6b10875b0fef08c5c2d176024c3116ae29
mrishikesh07 <rishikeshmasurkar7@gmail.com>
2026-03-10 14:36:31 +0530

    Added Task Header, some content and a Clickable AddTask Component

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,20 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import AddTask from './components/AddTask'
+
 
 function App() {
   
 
   return (
     <>
-      
+      <h1 className='text-2xl font-bold py-4 pl-6' >The Task Tracker</h1>
+      <p className='text-xl pl-6'>Hi there!</p>
+      <div className='flex flex-row items-center' >
+      <p className='text-xl pl-6'>Click</p>
+        <AddTask/>
+      <p className='text-xl my-2'>to add a new task</p>
+
+      </div>
     </>
   )
 }
